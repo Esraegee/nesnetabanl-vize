96ecb8a047a67eab386a20a38b82acb84c1c361b
Esraegee <95979136+Esraegee@users.noreply.github.com>
2021-12-23 20:49:13 +0300

    Add files via upload

---
+++ Nesne tabanlı programlama ödevi (Esra Ege ).cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace yemek_tarifiıu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yemekad;
+            string malzemeler;
+
+            Console.WriteLine("Yapmak istediğiniz yemeğin adını giriniz");
+            yemekad=Console.ReadLine();
+
+            Console.WriteLine("Yemek için gerekli malzemeleri giriniz");
+            malzemeler = Console.ReadLine();
+
+            Console.WriteLine("YEMEĞİN ADI" + yemekad);
+            Console.WriteLine("GEREKLİ MALZEMELER" + malzemeler);
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
